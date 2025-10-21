72a8c84427fb6864ef8f007f6160a2b679d8a157
WilliamOut <williamaraujo2707@gmail.com>
2025-10-21 18:44:43 -0300

    payment

--- dscommerce/src/main/java/com/devsuperior/dscommerce/entities/User.java
+++ dscommerce/src/main/java/com/devsuperior/dscommerce/entities/User.java
@@ -23,10 +23,13 @@ public class User {
     private String phone;
     private LocalDate birthDate;
     private String password;
-    
+
     @OneToMany(mappedBy = "client") // * nome do atributo la na outra classe
     private List<Order> orders = new ArrayList<>();
-    
+
+    public User() {
+    }
+
     public User(Long id, String name, String email, String phone, LocalDate birthDate, String password) {
         this.id = id;
         this.name = name;
@@ -39,36 +42,47 @@ public class User {
     public Long getId() {
         return id;
     }
+
     public void setId(Long id) {
         this.id = id;
     }
+
     public String getName() {
         return name;
     }
+
     public void setName(String name) {
         this.name = name;
     }
+
     public String getEmail() {
         return email;
     }
+
     public void setEmail(String email) {
         this.email = email;
     }
+
     public String getPhone() {
         return phone;
     }
+
     public void setPhone(String phone) {
         this.phone = phone;
     }
+
     public LocalDate getBirthDate() {
         return birthDate;
     }
+
     public void setBirthDate(LocalDate birthDate) {
         this.birthDate = birthDate;
     }
+
     public String getPassword() {
         return password;
     }
+
     public void setPassword(String password) {
         this.password = password;
     }
@@ -77,5 +91,4 @@ public class User {
         return orders;
     }
 
-    
 }
